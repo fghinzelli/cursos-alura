0db5ab0d5ab21a7e2e22378d31ba71028fa71bb9
Fernando Ghinzell <fghinzelli@gmail.com>
2017-03-27 16:49:46 -0300

    Django: migrations

--- django/connectedin/perfis/views.py
+++ django/connectedin/perfis/views.py
@@ -5,11 +5,5 @@ def index(request):
 	return render(request, 'perfis/index.html')
 
 def exibir(request, perfil_id):
-	perfil = Perfil()
-	if (perfil_id == '1'):
-		perfil = Perfil('Fernando Ghinzelli', 'fghinzelli@gmail.com', '7777', 'MP')
-	if perfil_id == '2':
-		perfil = Perfil('Romulo Henrique', 'romulo@romulo.com.br', '888888', 'Caelum')
-
-	print("Nome: " + perfil.nome)
+	perfil = Perfil.objects.get(id=perfil_id)
 	return render(request, 'perfis/perfil.html', {'perfil': perfil})
