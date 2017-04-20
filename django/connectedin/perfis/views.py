4bb3cbb6bd7813b32e208591048909d046a6a57c
Fernando Ghinzell <fghinzelli@gmail.com>
2017-04-20 16:58:52 -0300

    Connectedin: LIstar contatos

--- django/connectedin/perfis/views.py
+++ django/connectedin/perfis/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Perfil
+from .models import Perfil, Convite
 
 def index(request):
 
@@ -7,7 +7,9 @@ def index(request):
 
 def exibir(request, perfil_id):
 	perfil = Perfil.objects.get(id=perfil_id)
-	return render(request, 'perfis/perfil.html', {'perfil': perfil, 'perfil_logado': get_perfil_logado(request)})
+	perfil_logado = get_perfil_logado(request)
+	ja_eh_contato = perfil in perfil_logado.contatos.all()
+	return render(request, 'perfis/perfil.html', {'perfil': perfil, 'perfil_logado': get_perfil_logado(request), 'ja_eh_contato': ja_eh_contato})
 
 def convidar(request, perfil_id):
 	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
@@ -17,3 +19,8 @@ def convidar(request, perfil_id):
 
 def get_perfil_logado(request):
 	return Perfil.objects.get(id=1)
+
+def aceitar(request, convite_id):
+	convite = Convite.objects.get(id=convite_id)
+	convite.aceitar()
+	return redirect('index')
