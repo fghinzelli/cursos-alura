49ceb4fd6cb5bd90359637c265ce54f5d6f23c42
Fernando Ghinzell <fghinzelli@gmail.com>
2017-04-20 16:27:46 -0300

    Connectedin: listar convites

--- django/connectedin/perfis/views.py
+++ django/connectedin/perfis/views.py
@@ -3,11 +3,11 @@ from .models import Perfil
 
 def index(request):
 
-	return render(request, 'perfis/index.html', {'perfis': Perfil.objects.all()})
+	return render(request, 'perfis/index.html', {'perfis': Perfil.objects.all(), 'perfil_logado': get_perfil_logado(request)})
 
 def exibir(request, perfil_id):
 	perfil = Perfil.objects.get(id=perfil_id)
-	return render(request, 'perfis/perfil.html', {'perfil': perfil})
+	return render(request, 'perfis/perfil.html', {'perfil': perfil, 'perfil_logado': get_perfil_logado(request)})
 
 def convidar(request, perfil_id):
 	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
