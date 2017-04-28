dfe9145e8fbe21eb08273e6260e5999027bd3882
Fernando Ghinzell <fghinzelli@gmail.com>
2017-04-28 09:54:11 -0300

    Usuarios: Cadastro

--- django/connectedin/usuarios/views.py
+++ django/connectedin/usuarios/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 from django.views.generic.base import View
+from perfis.models import Perfil
+from usuarios.forms import RegistrarUsuarioForm
 
 class RegistrarUsuarioView(View):
 	template_name = 'registrar.html'
@@ -7,4 +11,29 @@ class RegistrarUsuarioView(View):
 		return render(request, self.template_name)
 
 	def post(self, request):
-		return render(request, self.template_name)
+
+		form = RegistrarUsuarioForm(request.POST)
+
+		if form.is_valid():
+
+			dados_form = form.data
+
+			# cria o usuario
+			usuario = User.objects.create_user(dados_form['nome'], dados_form['email'], dados_form['senha'])
+
+			# cria o perfil
+			perfil = Perfil(nome=dados_form['nome'],
+							#email=dados_form['email'],
+							telefone=dados_form['telefone'],
+							nome_empresa=dados_form['nome_empresa'],
+							usuario=usuario)
+
+			# grava no banco
+			perfil.save()
+
+			return redirect('index')
+
+		# so chega aqui se nao for valido
+		# vamos devolver o form para mostrar o formulario preenchido
+
+		return render(request, self.template_name, {'form': form})
