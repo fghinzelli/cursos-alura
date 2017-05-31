48a5af3fbc00384a67c392e83bb6b3a57417f811
Fernando Ghinzell <fghinzelli@gmail.com>
2017-05-31 17:35:07 -0300

    Python3: Inicio

---
+++ python3/adivinhacao.py
@@ -0,0 +1,22 @@
+total_tentativas = 3
+numero_secreto = 42
+rodada = 1
+
+while rodada <= total_tentativas:
+	print("Tentativa ", rodada, " de ", total_tentativas )
+	chute = input("Digite um número")
+	chute = int(chute)
+
+	acertou = numero_secreto == chute
+	menor = numero_secreto < chute
+	maior = numero_secreto > chute
+	if acertou:
+		print("Você acertou")
+		break
+	else:
+		if menor:
+			print("Você errou! O número é menor")
+		elif maior:
+			print("Você errou! O número é maior")
+	rodada += 1
+print("Fim do jogo")
