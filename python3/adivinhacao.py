70ee7b5e43d2a4c8687e5ce772417ed5cfe94ddc
Fernando Ghinzell <fghinzelli@gmail.com>
2017-06-01 17:29:25 -0300

    Alterado de while para for

--- python3/adivinhacao.py
+++ python3/adivinhacao.py
@@ -1,8 +1,8 @@
 total_tentativas = 3
 numero_secreto = 42
-rodada = 1
+total_tentativas = 3
 
-while rodada <= total_tentativas:
+for rodada in range(1, total_tentativas + 1):
 	print("Tentativa {} de {}".format(rodada, total_tentativas))
 	chute = input("Digite um número")
 	chute = int(chute)
@@ -18,5 +18,5 @@ while rodada <= total_tentativas:
 			print("Você errou! O número é menor")
 		elif maior:
 			print("Você errou! O número é maior")
-	rodada += 1
+
 print("Fim do jogo")
