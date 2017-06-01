a7f28b05c3716cc0e87c597eb7618aeee971e970
Fernando Ghinzell <fghinzelli@gmail.com>
2017-06-01 17:43:10 -0300

    Ajustes para for

--- python3/adivinhacao.py
+++ python3/adivinhacao.py
@@ -1,11 +1,18 @@
+print("************************************")
+print("* Bem vindo ao jogo de adivinhacao *")
+print("************************************")
 total_tentativas = 3
 numero_secreto = 42
 total_tentativas = 3
 
 for rodada in range(1, total_tentativas + 1):
 	print("Tentativa {} de {}".format(rodada, total_tentativas))
-	chute = input("Digite um número")
+	chute = input("Digite um número de 1 a 100: ")
+
 	chute = int(chute)
+	if(chute < 1 or chute > 100):
+		print("Você deve digitar um número entre 1 e 100!")
+		continue
 
 	acertou = numero_secreto == chute
 	menor = numero_secreto < chute
