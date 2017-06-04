82ec4aef7e99974cf355432ea6ec29f7e74b42ca
Fernando Ghinzelli <fghinzelli@gmail.com>
2017-06-04 16:56:17 -0300

    Python3: continue e break

--- python3/adivinhacao.py
+++ python3/adivinhacao.py
@@ -6,24 +6,24 @@ numero_secreto = 42
 total_tentativas = 3
 
 for rodada in range(1, total_tentativas + 1):
-	print("Tentativa {} de {}".format(rodada, total_tentativas))
-	chute = input("Digite um número de 1 a 100: ")
+    print("Tentativa {} de {}".format(rodada, total_tentativas))
+    chute = input("Digite um número de 1 a 100: ")
 
-	chute = int(chute)
-	if(chute < 1 or chute > 100):
-		print("Você deve digitar um número entre 1 e 100!")
-		continue
+    chute = int(chute)
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
-	acertou = numero_secreto == chute
-	menor = numero_secreto < chute
-	maior = numero_secreto > chute
-	if acertou:
-		print("Você acertou")
-		break
-	else:
-		if menor:
-			print("Você errou! O número é menor")
-		elif maior:
-			print("Você errou! O número é maior")
+    acertou = numero_secreto == chute
+    menor = numero_secreto < chute
+    maior = numero_secreto > chute
+    if acertou:
+        print("Você acertou")
+        break
+    else:
+        if menor:
+            print("Você errou! O número é menor")
+        elif maior:
+            print("Você errou! O número é maior")
 
 print("Fim do jogo")
