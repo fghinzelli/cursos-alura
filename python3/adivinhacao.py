27213dbd9b8286155d9237fdec0c1838bdbf2da8
Fernando Ghinzell <fghinzelli@gmail.com>
2017-06-05 13:28:19 -0300

    Inclusao de um novo arquivo

--- python3/adivinhacao.py
+++ python3/adivinhacao.py
@@ -4,8 +4,21 @@ print("************************************")
 print("* Bem vindo ao jogo de adivinhacao *")
 print("************************************")
 
-numero_secreto = random.randrange(1, 101)
-total_tentativas = 3
+numero_secreto = random.randrange(1, 100)
+total_tentativas = 0
+pontos = 1000
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if nivel == 1:
+    total_tentativas = 20
+elif nivel == 2:
+    total_tentativas = 10
+else:
+    total_tentativas = 5
 
 for rodada in range(1, total_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_tentativas))
@@ -20,12 +33,14 @@ for rodada in range(1, total_tentativas + 1):
     menor = numero_secreto < chute
     maior = numero_secreto > chute
     if acertou:
-        print("Você acertou")
+        print("Você acertou e fez {} pontos!".format(pontos))
         break
     else:
         if menor:
             print("Você errou! O número é menor")
         elif maior:
             print("Você errou! O número é maior")
+        pontos_perdidos = round(abs(numero_secreto - chute) / 3)
+        pontos = pontos - pontos_perdidos
 
 print("Fim do jogo")
