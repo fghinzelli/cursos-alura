898e839d98a504149f50b13aca0e00cf76d3295b
Fernando Ghinzell <fghinzelli@gmail.com>
2017-06-06 17:26:10 -0300

    Jogo da forca: Inclusao de lista no jogo

--- python3-parte2/forca.py
+++ python3-parte2/forca.py
@@ -4,10 +4,12 @@ def jogar():
 	print("*********************************")
 
 	palavra_secreta = "banana"
+	letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 	enforcou = False
 	acertou = False
 
-
+	print(letras_acertadas)
+	
 	while(not acertou and not enforcou):
 		chute = input("Qual letra? ")
 		chute = chute.strip()
@@ -15,10 +17,10 @@ def jogar():
 		index = 0
 		for letra in palavra_secreta:
 			if (chute.upper() == letra.upper()):
-				print("Encontrei a letra {} na posicao {}".format(letra, index))
+				letras_acertadas[index] = letra
 			index = index + 1
 
-		print("Jogando...")
+		print(letras_acertadas)
 
 
 	print("Fim do jogo")
