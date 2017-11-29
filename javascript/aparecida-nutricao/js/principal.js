3c0a25272cce515a85b90456b9aedbc44e827a03
Fernando Ghinzelli <fghinzelli@gmail.com>
2017-11-29 10:51:49 -0200

    Teste de imc no primeiro paciente

--- javascript/aparecida-nutricao/js/principal.js
+++ javascript/aparecida-nutricao/js/principal.js
@@ -9,6 +9,22 @@ var tdImc = paciente.querySelector(".info-imc");
 var peso = parseInt(tdPeso.textContent);
 var altura = parseInt(tdAltura.textContent);
 
-var imc = peso / (altura * altura);
+var pesoEhValido = true;
+var alturaEhValida = true;
 
-tdImc.textContent = imc;
+if (peso <= 0 || peso > 1000) {
+	console.log("Peso inválido");
+	tdImc.textContent = "Peso inválido!";
+	pesoEhValido = false;
+}
+
+if (altura <= 0 || altura > 3) {
+	console.log("Altura inválida");
+	tdImc.textContent = "Altura Inválida";
+	alturaEhValida = false;
+}
+
+if (pesoEhValido && alturaEhValida) {
+	var imc = peso / (altura * altura);
+	tdImc.textContent = imc;
+}
