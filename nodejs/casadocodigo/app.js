827baeb90573dae162e9724381e545527ac8793c
fghinzelli <fghinzelli@gmail.com>
2018-09-27 21:15:36 -0300

    Nodejs: Isolando modulos

--- nodejs/casadocodigo/app.js
+++ nodejs/casadocodigo/app.js
@@ -1,7 +1,5 @@
-var express = require('express');
-var app = express();
 
-app.set('view engine', 'ejs');
+var app = require('./config/express')();
 
 app.get('/produtos', function(req, res){
     res.render('produtos/lista');
