06ed3e49c8aa76db5c6c73a77300eddadf44c35b
Fernando Ghinzelli <fghinzelli@gmail.com>
2018-12-05 21:31:10 -0200

    Testes de herança

---
+++ javascript2/conta/Conta.js
@@ -0,0 +1,13 @@
+class Conta {
+    constructor(saldo) {
+        this._saldo = saldo;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    atualiza(taxa) {
+        throw new Error('Método atualiza não foi instanciado');
+    }
+ }
