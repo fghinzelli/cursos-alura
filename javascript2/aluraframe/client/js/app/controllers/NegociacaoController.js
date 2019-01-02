09e99ce10fcdd1314ae9d728c5f4c3ea184696ee
Fernando Ghinzelli <fghinzelli@gmail.com>
2019-01-02 10:58:01 -0200

    Javascript2: Organização do arquivo NegociacaoService.js

--- javascript2/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript2/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -24,26 +24,16 @@ class NegociacaoController {
     }
 
     importaNegociacoes() {
+        let service = new NegociacaoService()
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+            if(erro) {
+                this._mensagem.texto = erro;
+                return;
+            } 
 
-        let xhr = new XMLHttpRequest();
-
-        xhr.open('GET', 'negociacoes/semanax');
-
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState == 4) {
-                if(xhr.status == 200) {
-                    JSON.parse(xhr.responseText)
-                      .map(objeto=> new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
-                      .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao))
-                      this._mensagem.texto = 'Negociações importadas com sucesso.';
-                } else {
-                    console.log(xhr.responseText);
-                    this._mensagem.texto = 'Não foi possível obter as negociações.';
-                }
-            }
-        }
-
-        xhr.send();
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao))
+            this._mensagem.texto = 'Negociações importadas com sucesso.';
+        });
     }
 
     apaga() {
