82fba7a7c64eef27c8e38c45280567335b73d172
Fernando Ghinzelli <fghinzelli@gmail.com>
2019-01-20 23:53:24 -0200

    AngularJS - Main e FotosController

---
+++ angularjs/alurapic/public/js/controllers/fotos-controller.js
@@ -0,0 +1,6 @@
+angular.module('alurapic').controller('FotosController', function($scope) {
+    $scope.foto = {
+        url: "http://fundosanimais.com/Minis/leoes.jpg",
+        titulo: "Leão"
+    }
+});
