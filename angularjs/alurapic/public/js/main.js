b7fd7fb87a8032617e6f50a51f390f3f2df9e58c
Fernando Ghinzelli <fghinzelli@gmail.com>
2019-02-19 07:14:50 -0300

    AngularJS: Form - primeira parte

--- angularjs/alurapic/public/js/main.js
+++ angularjs/alurapic/public/js/main.js
@@ -6,5 +6,10 @@ angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
         controller: 'FotosController'
     });
 
+    $routeProvider.when('/fotos/new', {
+        templateUrl: 'partials/foto.html',
+        controller: 'FotoController'
+    });
+
     $routeProvider.otherwise({redirectTo: '/fotos'});
 });
