e8d969770c1108c9310e473a4742c73ac11928f3
Fernando Ghinzelli <fghinzelli@gmail.com>
2019-02-20 07:14:49 -0300

    AngularJS: Validação de form

--- angularjs/alurapic/public/js/controllers/foto-controller.js
+++ angularjs/alurapic/public/js/controllers/foto-controller.js
@@ -1,7 +1,17 @@
-angular.module('alurapic').controller('FotoController', function($scope) {
+angular.module('alurapic').controller('FotoController', function($scope, $http) {
     $scope.foto = {};
 
     $scope.submeter = function() {
-        console.log($scope.foto);
+        if ($scope.formulario.$valid) {
+            $http.post('/v1/fotos', $scope.foto)
+            .success(function(){
+                $scope.foto = {};
+                $scope.mensagem = 'Foto adicionada com sucesso'; 
+            }).error(function(erro) {
+                console.error(erro);
+                $scope.mensagem = 'Não foi possível cadastrar a foto';
+            });
+        }
+        
     }
 });
