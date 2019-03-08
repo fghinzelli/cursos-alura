0a231ff0b91848a59c94300b088cb7cad3bc96d6
Fernando Ghinzelli <fghinzelli@gmail.com>
2019-03-08 01:07:09 -0300

    ConnectionFactory: Ajustes

--- javascript3/aluraframe/client/js/app/services/ConnectionFactory.js
+++ javascript3/aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,6 +1,59 @@
-class ConnectionFactory {
+var ConnectonFactory = (function() { 
+    const stores = ['negociacoes'];
+    const version = 4;
+    const dbName = 'aluraframe';
 
-    static getConnection() {
-        
+    var connection = null;
+    var close = null;
+
+    return class ConnectionFactory {
+
+        constructor() {
+
+            throw new Error('Não é possível intanciar esta classe');
+        }
+
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+                let openRequest = window.indexedDB.open(dbName, version);
+
+                openRequest.onupgradeneeded = e => {
+                    
+                    ConnectionFactory._createStores(e.target.result);
+                };
+
+                openRequest.onsuccess = e => {
+                    
+                    if (!connection) {
+                        connection = e.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = function() {
+                            throw new Error('Você não pode fechar a conexão');
+                        }
+                    }
+                    resolve(connection);
+                };
+
+                openRequest.onerror = e => {
+
+                    reject(e.target.error.name);
+                };
+            })
+        }      
+
+        static _createStores(connection) {
+            stores.forEach(store => {
+                if(connection.objectStoreNames.contains(store)) 
+                    connection.deleteObjectStore(store);
+                connection.createObjectStore(store, {autoincrement: true});
+            });
+        }
+
+        static closeConnection() {
+            if (connection) {
+                close();
+                connection = null;
+            }
+        }
     }
-}
+})();
