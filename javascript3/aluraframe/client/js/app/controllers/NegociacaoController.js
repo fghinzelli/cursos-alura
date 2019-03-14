54a97356c2cc1f9d60dbc6457e8ff26e019eaf2f
Fernando Ghinzelli <fghinzelli@gmail.com>
2019-03-14 20:56:28 -0300

    Funcoes filter e some para filtro de objetos em listas js

--- javascript3/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ javascript3/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -57,6 +57,11 @@ class NegociacaoController {
         let service = new NegociacaoService();
         service
             .obterNegociacoes()
+            .then(negociacoes => 
+                negociacoes.filter(negociacao => 
+                    !this._listaNegociacoes.negociacoes.some(negociacaoExistente =>
+                        JSON.stringify(negociacaoExistente) == JSON.stringify(negociacao)))
+            )
             .then(negociacoes => negociacoes.forEach(negociacao => {
                 this._listaNegociacoes.adiciona(negociacao);
                 this._mensagem.texto = 'Negociações do período importadas'   
