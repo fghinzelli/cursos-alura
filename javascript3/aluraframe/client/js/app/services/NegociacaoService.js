813c0940007f205f5c2499114d524fe91f554cfd
Fernando Ghinzelli <fghinzelli@gmail.com>
2019-03-15 11:02:57 -0300

    Correção

--- javascript3/aluraframe/client/js/app/services/NegociacaoService.js
+++ javascript3/aluraframe/client/js/app/services/NegociacaoService.js
@@ -119,11 +119,11 @@ class NegociacaoService {
             .then(negociacoes => 
                 negociacoes.filter(negociacao => 
                     !listaAtual.some(negociacaoExistente =>
-                        negociacao.isEquals(negociacaoExistente))
+                        negociacao.isEquals(negociacaoExistente)))
             ).catch(erro => {
                 console.log(erro);
                 throw new Error('Não foi possível importar as negociações');
-            })
+            });
     }
 }
 
