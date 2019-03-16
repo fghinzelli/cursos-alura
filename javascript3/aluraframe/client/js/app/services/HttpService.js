e633e62fc3448bfb8d6525695cb0016e85e1403e
Fernando Ghinzelli <fghinzelli@gmail.com>
2019-03-16 11:34:25 -0300

    Versão do POST usando XHR

--- javascript3/aluraframe/client/js/app/services/HttpService.js
+++ javascript3/aluraframe/client/js/app/services/HttpService.js
@@ -13,4 +13,22 @@ class HttpService {
             .then(res => res.json());
         
     }
+
+    post(url, dado) {
+
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', url, true);
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4) {
+                    if(xhr.status == 200) {
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+                        reject(xhr.responseText);
+                    }
+                }        
+            }
+            xhr.send(JSON.stringfy(dado));
+        });
+    }
 }
