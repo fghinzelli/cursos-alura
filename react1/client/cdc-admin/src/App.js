1f8e6eb178bb319762133ae74782b88b4274e2a1
Fernando Ghinzelli <fghinzelli@gmail.com>
2019-03-25 06:24:06 -0300

    Routes

--- react1/client/cdc-admin/src/App.js
+++ react1/client/cdc-admin/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import AutorBox from './componentes/Autor';
+import {BrouserRouter as Router, Link, Route} from 'react-router-dom'
 
 
 class App extends Component {
@@ -9,32 +10,34 @@ class App extends Component {
   render() {
     return (
       <div id="layout">
-        <a href="#menu" id="menuLink" className="menu-link">
-            <span></span>
-        </a>
+        <Router >
+          <a href="#menu" id="menuLink" className="menu-link">
+              <span></span>
+          </a>
 
-        <div id="menu">
-            <div className="pure-menu">
-                <a className="pure-menu-heading" href="#">Company</a>
+          <div id="menu">
+              <div className="pure-menu">
+                  <a className="pure-menu-heading" href="#">Company</a>
 
-                <ul className="pure-menu-list">
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-                    <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livros</a></li>
-                </ul>
-            </div>
-        </div>
+                  <ul className="pure-menu-list">
+                      <li className="pure-menu-item"><Link to="/">Home</Link></li>
+                      <li className="pure-menu-item"><Link to='/autor'>Autor</Link></li>
+                      <li className="pure-menu-item"><Link to='/livros'>Livros</Link></li>
+                  </ul>
+              </div>
+          </div>
 
-        <div id="main">
+          <div id="main">
+            <Route exact path='/'>
             <div className="header">
-                <h1>Cadastro de Autores</h1>
-            </div>
-
-            <div className="content" id="content">
-                <AutorBox />
-            </div>
-        </div>
-    </div>
+                  <h1>Bem vindo ao sistema</h1>
+              </div>
+            </Route>
+            <Route path="/autor" component={AutorBox}></Route>
+          </div>
+          </Router>
+      </div>
+      
     );
   }
 }
--- react1/client/cdc-admin/src/index.js
+++ react1/client/cdc-admin/src/index.js
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AutorBox from './componentes/Autor'
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
 
 
 ReactDOM.render(
-    (<BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/autor" />
-            <Route path="/livro" />
-        </Switch>
-    </BrowserRouter>),
+    <App />, 
     document.getElementById('root')
 );
 
