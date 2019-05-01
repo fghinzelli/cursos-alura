489df33cde6bebe8010f68ab293e37036424be02
Fernando Ghinzelli <fghinzelli@gmail.com>
2019-05-01 08:57:35 -0300

    Componentizacao da timeline e item

--- react2/instalura/src/App.js
+++ react2/instalura/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Timeline from './componentes/Timeline';
 import Header from './componentes/Header';
 
--- react2/instalura/src/componentes/FotoItem.js
+++ react2/instalura/src/componentes/FotoItem.js
@@ -20,31 +20,30 @@ class FotoInfo extends Component {
         return (
             <div className="foto-info">
               <div className="foto-info-likes">
-
-                <a href="#">
-                  alots_ssa
-                </a>
-
-                ,
-
-                <a href="#">
-                  rafael_rollo
-                </a> 
-
+                {
+                  this.props.foto.likers.map(liker => <a href="#">{liker.login}, </a>)
+                }
                  curtiram
-
               </div>
 
               <p className="foto-info-legenda">
                 <a className="foto-info-autor">autor </a>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, illo?
+                {this.props.foto.comentario}
               </p>
 
               <ul className="foto-info-comentarios">
-                <li className="comentario">
-                  <a className="foto-info-autor">seguidor </a>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ad, molestiae.
-                </li>
+                
+                  {
+                    this.props.foto.comentarios.map(comentario => {
+                      return (
+                        <li className="comentario">
+                          <a className="foto-info-autor">seguidor </a>
+                          {comentario.texto}
+                        </li>
+                      );
+                    })
+                  }
+                  
                 <li className="comentario">
                   <a className="foto-info-autor">seguidor </a>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt cumque earum molestias voluptatem modi nihil sit magnam ratione eveniet distinctio magni error asperiores dignissimos tempora expedita, laborum ex soluta hic maiores veritatis deserunt.
@@ -64,14 +63,14 @@ class FotoHeader extends Component {
         return (
             <header className="foto-header">
               <figure className="foto-usuario">
-                <img src="https://s3.amazonaws.com/caelum-online-public/react-native-parte-2/images/adittional-resources/profile-photo-rafael.jpg" alt="foto do usuario"/>
+                <img src={this.props.foto.urlPerfil} alt="foto do usuario"/>
                 <figcaption className="foto-usuario">
                   <a href="#">
-                    alots
+                    {this.props.foto.loginUsuario}
                   </a>  
                 </figcaption>
               </figure>
-              <time className="foto-data">03/10/2016 20:13</time>
+              <time className="foto-data">{this.props.foto.horario}</time>
             </header>            
         );
     }
@@ -81,9 +80,9 @@ export default class FotoItem extends Component {
     render(){
         return (
           <div className="foto">
-            <FotoHeader/>
-            <img alt="foto" className="foto-src" src="https://s3.amazonaws.com/caelum-online-public/react-native-parte-2/images/adittional-resources/photo-1.jpg"/>
-            <FotoInfo/>
+            <FotoHeader foto={this.props.foto}/>
+            <img alt="foto" className="foto-src" src={this.props.foto.urlFoto}/>
+            <FotoInfo foto={this.props.foto}/>
             <FotoAtualizacoes/>
           </div>            
         );
