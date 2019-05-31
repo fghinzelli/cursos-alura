547ec1eec45dcb96840b464dba6612be99985e02
Fernando Ghinzelli <fghinzelli@gmail.com>
2019-05-31 12:30:29 -0300

    Python OO - Exercises!

--- python-oo/conta.py
+++ python-oo/conta.py
@@ -1,19 +1,37 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
-
-    def depositar(self, valor):
-        self.saldo += valor
-
-    def sacar(self, valor):
-        self.saldo -= valor
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("{0}, seu saldo é de R$ {1}".format(self.titular, self.saldo))
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
-    def pega_saldo(self):
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+    
+    @property
+    def saldo(self):
         return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
