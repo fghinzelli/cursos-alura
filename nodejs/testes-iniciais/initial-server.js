ff1458a91548969c96ed5d9e716068412c2fd3de
Fernando Ghinzelli <fghinzelli@gmail.com>
2019-09-19 13:12:00 -0300

    Reset do diretorio node

--- nodejs/testes-iniciais/initial-server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http')
-
-var server = http.createServer(function(req, res){
-	res.end('<h1>Listagem de Produtos</h1>');
-
-});
-server.listen(3000);
-console.log('servidor rodando')
