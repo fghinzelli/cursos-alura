f77766755dc767d42eaac842feffbb6c02ac365a
Fernando Ghinzelli <fghinzelli@gmail.com>
2019-09-29 08:17:29 -0300

    app nodejs

---
+++ nodejs/casadocodigo/src/app/rotas/rotas.js
@@ -0,0 +1,31 @@
+
+const db = require('../../config/database');
+
+module.exports = (app) => {
+    app.get('/', function(req, resp) {
+        resp.send(
+            `
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1> Casa do Código </h1>
+                    </body>
+                </html>
+            `
+        );
+    });
+    
+    app.get('/livros', function(req, resp) {
+        db.all(`SELECT * FROM LIVROS`, function(error, resultados){
+            resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: resultados
+                }
+            );
+        });
+        
+    });
+};
