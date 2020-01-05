877afb1f7274bc8ba5e2df2d8239192a4040ceb8
Fernando Ghinzelli <fghinzelli@gmail.com>
2020-01-05 14:37:46 -0300

    Conclusão do curso node fundamentos

--- nodejs/casadocodigo/src/app/infra/livros-dao.js
+++ nodejs/casadocodigo/src/app/infra/livros-dao.js
@@ -3,7 +3,7 @@ class LivrosDao {
         this._db = db;
     }
 
-    lista(callback) {
+    lista() {
         return new Promise((resolve, reject) => {
             this._db.all(
                 'SELECT * FROM livros',
@@ -16,7 +16,82 @@ class LivrosDao {
                 }
             );
         });
+    }
 
+    adiciona(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO LIVROS (
+                titulo, preco, descricao
+                ) VALUES (?, ?, ?)   
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ],
+                function(err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro');
+                    } 
+                    resolve()
+                }
+                )
+
+        });
+    }
+
+    buscarPorId(id) {
+        return new Promise((resolve, reject) => {
+            this._db.get(`
+                SELECT * FROM livros WHERE id = ?`, 
+                [id],
+                (erro, livro) => {
+                    if (erro) {
+                        return reject('Não foi possível encontrar o livro')
+                    }
+                    return resolve(livro);
+            })
+        })
+    }
+
+    atualiza(livro) {
+        console.log(livro);
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                UPDATE livros SET 
+                    titulo = ?,
+                    preco = ?,
+                    descricao = ?
+                WHERE id = ?
+            `,
+            [
+                livro.titulo,
+                livro.preco,
+                livro.descricao,
+                livro.id
+            ], err => {
+                if (err) {
+                    return reject('Não foi possível atualizar o livro')
+                }
+                resolve();
+            })
+        })
+    }
+
+    remove(id) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                DELETE FROM livros WHERE id = ?`, 
+                [id], 
+                err => {
+                    if (err) {
+                        return reject('Não foi possível excluir o livro')
+                    }
+                    resolve();
+                })
+        })
     }
 }
 
--- nodejs/casadocodigo/src/app/rotas/rotas.js
+++ nodejs/casadocodigo/src/app/rotas/rotas.js
@@ -18,5 +18,40 @@ module.exports = (app) => {
             );
         }).catch(error => console.log(error));
     });
+
+    app.get('/livros/form', (req, res) => {
+        res.marko(require('../views/livros/form/form.marko'), { livro: {}});
+    })
+
+    app.post('/livros', (req, res) => {
+        const livrosDao = new LivrosDao(db);
+        livrosDao.adiciona(req.body)
+        .then(res.redirect('/livros'))
+        .catch(erro => console.log(erro));
+    })
+
+    app.get('/livros/form/:id', (req, res) => {
+        const livrosDao = new LivrosDao(db);
+        livrosDao.buscarPorId(req.params.id)
+        .then(livro => {
+            res.marko(require('../views/livros/form/form.marko'),
+            { livro: livro })
+        })
+        .catch(erro => console.log(erro))
+    })
+
+    app.put('/livros', (req, res) => {
+        const livrosDao = new LivrosDao(db);
+        livrosDao.atualiza(req.body)
+        .then(res.redirect('/livros'))
+        .catch(erro => console.log(erro))
+    })
+
+    app.delete('/livros/:id', (req, res) => {
+        const livrosDao = new LivrosDao(db);
+        livrosDao.remove(req.params.id)
+        .then(() => res.status(200).end())
+        .catch(erro => console.log(erro))
+    })
 };
 
