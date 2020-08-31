535c257d9b0483bd5b008c4f63b001092d13996c
Fernando Ghinzelli <fghinzelli@gmail.com>
2020-08-31 10:44:57 -0300

    Correção connectionFactory

--- node-api-rest1/persistencia/connectionFactory.js
+++ node-api-rest1/persistencia/connectionFactory.js
@@ -1,13 +1,23 @@
 var mysql = require('mysql');
 
+const casa = {
+  host: 'localhost',
+  user: 'financas',
+  port: 3306,
+  password: 'financas',
+  database: 'financas'
+};
+
+const pmcs = {
+  host: '10.241.12.6',
+  user: 'mariadb-user',
+  port: 3307,
+  password: 'mariadb-pwd',
+  database: 'mariadb-localhost'
+};
+
 function createDBConnection(){
-  return mysql.createConnection({
-    host: 'localhost',
-    user: 'financas',
-    port: 3306,
-    password: 'financas',
-    database: 'financas'
-  });
+  return mysql.createConnection(pmcs);
 }
 
 module.exports = function() {
