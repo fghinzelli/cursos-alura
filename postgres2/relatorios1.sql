6b8fcedf015ed9cabf15cc0137bec5b57e55c9c3
Fernando Ghinzelli <fghinzelli@gmail.com>
2020-10-07 07:33:41 -0300

    UPDATE

--- postgres2/relatorios1.sql
+++ postgres2/relatorios1.sql
@@ -4,3 +4,10 @@ INNER JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
 GROUP BY 1,2
 ORDER BY numero_cursos DESC
 LIMIT 1;
+
+
+SELECT curso.nome, COUNT(aluno_curso.aluno_id) numero_alunos
+FROM curso
+INNER JOIN aluno_curso ON aluno_curso.curso_id = curso.id
+GROUP BY curso.nome 
+ORDER BY numero_alunos
