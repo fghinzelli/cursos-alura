512838bcef0c4548e1fcebf8fcae0f04c49789d3
Fernando Ghinzelli <fghinzelli@gmail.com>
2020-10-22 07:26:09 -0300

    React and vim

--- react-revisao/src/App.js
+++ react-revisao/src/App.js
@@ -4,11 +4,24 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notas: []
+    }
+  }
+
+  criarNota(titulo, texto) {
+    const newNota = {'titulo': titulo, 'texto': texto};
+    const newNotas = {notas: [...this.state.notas, newNota]}
+    this.setState(newNotas);
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
       </section>
     );
   }
--- react-revisao/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ react-revisao/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,31 +2,50 @@ import React, { Component } from "react";
 import "./estilo.css";
 class FormularioCadastro extends Component {
 
-  constructor() {
-    super();
-    this.titulo = ''
+  constructor(props) {
+    super(props);
+    this.titulo = '';
+    this.texto = '';
   }
 
-  handlerTitulo(event) {
-    console.log(event.target.value);
+  _handleTitulo(event) {
+    event.stopPropagation();
     this.titulo = event.target.value;
+  }
+
+  _handleTexto(event) {
+    event.stopPropagation();
+    this.texto = event.target.value;
+  }
+
+  _handleEnviar(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.props.criarNota(this.titulo, this.texto);
   }
 
   render() {
     return (
-      <form className="form-cadastro ">
+      <form 
+        className="form-cadastro "
+        onSubmit={this._handleEnviar.bind(this)}
+      >
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
-          onChange={this.handlerTitulo.bind(this)}
+          onChange={this._handleTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this._handleTexto.bind(this)}
         />
-        <button className="form-cadastro_input form-cadastro_submit">
+        <button 
+          className="form-cadastro_input form-cadastro_submit"
+          
+        >
           Criar Nota
         </button>
       </form>
