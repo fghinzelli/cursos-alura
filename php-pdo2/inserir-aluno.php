d39192a8053dc11dff1b8aabed81a7b60efad807
Fernando Ghinzelli <fghinzelli@gmail.com>
2020-11-26 07:22:50 -0300

    Php-pdo: Prepared Statement

--- php-pdo2/inserir-aluno.php
+++ php-pdo2/inserir-aluno.php
@@ -7,10 +7,18 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Fernando Ghinzelli', new \DateTimeImmutable('1985-08-14'));
+$student = new Student(
+  null, 
+  'Bernardo Carissimi Ghinzelli',  
+  new \DateTimeImmutable('2021-01-14'));
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
-
-// echo $sqlInsert;
-
-var_dump($pdo->exec($sqlInsert));
+// $sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(':name', $student->name());
+$statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
+// statement->bindParam(2, $student->name()); // Parameter send by referency
+$statement->execute();
+if ($statement->execute()) {
+  echo "Aluno incluído";
+}
