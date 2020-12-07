e0a81bccbc2679011b12f3f2937d12109e1612fe
Fernando Ghinzelli <fghinzelli@gmail.com>
2020-12-07 07:06:53 -0300

    Injeção de dependencia

--- php-pdo2/teste.php
+++ php-pdo2/teste.php
@@ -3,10 +3,12 @@
 use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
 use DateTime;
 use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
 
 require_once 'vendor/autoload.php';
 
-$studentRepository = new PdoStudentRepository();
+$connection = ConnectionCreator::createConnection();
+$studentRepository = new PdoStudentRepository($connection);
 
 $newStudent = new Student($id = 1, $name = 'Fernando Ghinzelliiii', $birthDate = new DateTime('1986-06-11'));
 $studentRepository->save($newStudent);
