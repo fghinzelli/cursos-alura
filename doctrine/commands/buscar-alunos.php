39f4ba3e7e5a6b79d03678aebe23c9c97c21d61d
Fernando Ghinzelli <fghinzelli@gmail.com>
2021-02-16 18:49:53 -0300

    OneToMany relation

--- doctrine/commands/buscar-alunos.php
+++ doctrine/commands/buscar-alunos.php
@@ -1,6 +1,7 @@
 <?php
 
 use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Entity\Telefone;
 use Alura\Doctrine\Helper\EntityManagerFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -16,9 +17,17 @@ $alunoList = $alunoRepository->findAll();
 // $alunoList = $alunoRepository->findBy([], ['nome' => 'DESC'], 2);
 
 foreach($alunoList as $aluno) {
+    $telefones = $aluno
+        ->getTelefones()
+        ->map(function (Telefone $telefone) {
+            return $telefone->getNumero();
+    })
+    ->toArray();
+
     echo "\nID: {$aluno->getId()}\nNome: {$aluno->getNome()}\n\n";
+    echo "Telefones: " . implode(', ', $telefones) . "\n";
 }
 
-$nico = $alunoRepository->find(4);
-echo "\n{$nico->getNome()}\n";
+// $nico = $alunoRepository->find(4);
+// echo "\n{$nico->getNome()}\n";
 
--- doctrine/commands/criar-aluno.php
+++ doctrine/commands/criar-aluno.php
@@ -1,15 +1,27 @@
 <?php
 
 use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Entity\Telefone;
 use Alura\Doctrine\Helper\EntityManagerFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$entityManageFactory = new EntityManagerFactory();
+$entityManager = $entityManageFactory->getEntityManager();
+
 $aluno = new Aluno();
 $aluno->setNome($argv[1]);
 
-$entityManageFactory = new EntityManagerFactory();
-$entityManager = $entityManageFactory->getEntityManager();
+for ($i = 2; $i < $argc; $i++) {
+  $numeroTelefone = $argv[$i];
+  $telefone = new Telefone();
+  $telefone->setNumero($numeroTelefone);
+
+  // $entityManager->persist($telefone);
+  
+  $aluno->addTelefone($telefone);
+}
+
 
 $entityManager->persist($aluno);
 
