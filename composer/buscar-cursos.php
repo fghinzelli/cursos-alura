06bf07c79cb590a03b5f28cf0bd46e88d3129136
Fernando Ghinzelli <fghinzelli@gmail.com>
2021-03-21 23:19:11 -0300

    Composer: OO

--- composer/buscar-cursos.php
+++ composer/buscar-cursos.php
@@ -1,8 +1,19 @@
 <?php
 
+require 'vendor/autoload.php';
+require 'src/Buscador.php';
+
+use Alura\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client();
-$resposta = $client->request('GET', 'http://alura.com.br/cursos-online-programacao/php');
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
 
-$html = $resposta->getBody();
+$buscador = new Buscador($client, $crawler);
+
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach($cursos as $curso) {
+  echo $curso . PHP_EOL;
+}
