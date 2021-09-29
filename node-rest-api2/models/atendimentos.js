92e32042c195f5e1f32a095a5b8fcb580337e3a9
Fernando Ghinzelli <fghinzelli@gmail.com>
2021-09-29 06:43:47 -0300

    Atualizacao

--- node-rest-api2/models/atendimentos.js
+++ node-rest-api2/models/atendimentos.js
@@ -3,9 +3,12 @@ const conexao = require('../infraestrutura/conexao');
 class Atendimentos {
   
   adiciona(atendimento) {
+
+    const dataCriacao = new Date();
+    const atendimentoDatado = {...atendimento, dataCriacao}
     const sql = 'INSERT INTO Atendimentos SET ?'
     
-    conexao.query(sql, atendimento, (erro, resultados) => {
+    conexao.query(sql, atendimentoDatado, (erro, resultados) => {
       if (erro) {
         console.log(erro)
       } else {
