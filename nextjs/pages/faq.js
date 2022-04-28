b4992a493179a8e8b3053a93112ee35c9d34265f
fghinzelli <fghinzelli@gmail.com>
2022-04-27 23:47:52 -0300

    First commit

---
+++ nextjs/pages/faq.js
@@ -0,0 +1,14 @@
+import Link from '../src/components/Link'
+
+
+export default function FaqPage() {
+    return (
+        <div>
+            <h1>Alura Cases - FAQ</h1>
+            <Link href="/">
+                Ir para Home
+            </Link>
+            
+        </div>
+    )
+}
