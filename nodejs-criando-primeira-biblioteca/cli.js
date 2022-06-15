6a482aad636f55bad37f83edd289b66f228b18d2
fghinzelli <fghinzelli@gmail.com>
2022-06-15 00:26:22 -0300

    Aula 5

--- nodejs-criando-primeira-biblioteca/cli.js
+++ nodejs-criando-primeira-biblioteca/cli.js
@@ -1,12 +1,16 @@
 import pegaArquivo from './index.js';
 import chalk from 'chalk';
+import validaURLs from './http-validacao.js';
 
 const caminho = process.argv
 
 async function processaTexto(caminhoDeArquivo) {
     const resultado = await pegaArquivo(caminhoDeArquivo[2])
-    console.log(chalk.yellow('Lista de links'), resultado)
-
+    if (caminhoDeArquivo[3] === 'validar') {
+        console.log(chalk.yellow('links validados'), await validaURLs(resultado))
+    } else {
+        console.log(chalk.yellow('lista de links'), resultado)
+    }
 }
 
 processaTexto(caminho);
