5d7938bbfb506589f85c47c03bebc3e2fdc2be45
fghinzelli <fghinzelli@gmail.com>
2022-07-29 00:19:54 -0300

    Capítulos 1, 2 3 e 4

--- javascript-aluramid/main.js
+++ javascript-aluramid/main.js
@@ -1,3 +1,15 @@
-document.querySelector('.tecla_pom').addEventListener('click', function() {
-    alert('kkkk')
-});
+function tocaSom(id) {
+    document.querySelector(id).play();
+}
+
+const listaTeclas = document.querySelectorAll('.tecla');
+
+for(let i = 0; i < listaTeclas.length; i++) {
+    const tecla = listaTeclas[i];
+    const classe = tecla.classList[1];
+    const idAudio = `#som_${classe}`;
+
+    tecla.onclick = function() {
+        tocaSom(idAudio)
+    };
+}
