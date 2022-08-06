f338e9c65b8d1cef84d6dc5f6f1dfbb74b73e294
fghinzelli <fghinzelli@gmail.com>
2022-08-05 23:54:39 -0300

    Javascript data validation

---
+++ javascript-validate-form/js/validacao.js
@@ -0,0 +1,29 @@
+const validadores = {
+    dataNascimento: input => validaDataNascimento(input)
+}
+
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[input.tipoDeInput]) {
+        validadores[input.tipoDeInput](input)
+    }
+
+}
+
+
+function validaDataNascimento(input) {
+    const dataRecebida = new Date(input.value);
+    let mensagem = '';
+    if (!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior de 18 anos para se cadastrar.'
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function maiorQue18(data) {
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth() , data.getUTCDate());
+
+    return dataMais18 <= dataAtual;
+;}
