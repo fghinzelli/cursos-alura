29a5e89b4d2790101176179c29583d579f7b61c2
fghinzelli <fghinzelli@gmail.com>
2023-09-06 00:06:56 -0300

    Get Fotos

--- mean/alurapic/app/api/fotos.js
+++ mean/alurapic/app/api/fotos.js
@@ -1,12 +1,23 @@
 var api = {}
+var fotos = [
+  {_id: 1, titulo: 'Leão', url: 'https://cdn.leroymerlin.com.br/products/quadro_leao_bronze_100x100cm_90658085_0001_1800x1800.jpg'},
+  {_id: 2, titulo: 'Leão 2', url: 'https://cdn.leroymerlin.com.br/products/quadro_leao_bronze_100x100cm_90658085_0001_1800x1800.jpg'}
+];
 
 api.lista = (req, res) => {
-  var fotos = [
-    {_id: 1, titulo: 'Leão', url: 'https://cdn.leroymerlin.com.br/products/quadro_leao_bronze_100x100cm_90658085_0001_1800x1800.jpg'},
-    {_id: 2, titulo: 'Leão 2', url: 'https://cdn.leroymerlin.com.br/products/quadro_leao_bronze_100x100cm_90658085_0001_1800x1800.jpg'}
-  ];
+  
 
   res.json(fotos);
+}
+
+api.buscarPorId = (req, res) => {
+  var id = req.params.id;
+  return res.json(fotos.filter(item => {
+    console.log(id)
+    console.log(item._id)
+    
+
+  }))
 
 }
 module.exports = api;
--- mean/alurapic/app/routes/foto.js
+++ mean/alurapic/app/routes/foto.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
   var api = app.api.fotos;
   app.get('/v1/fotos', api.lista)
-
+  app.get('/v1/fotos/:id', api.buscarPorId)
 }
