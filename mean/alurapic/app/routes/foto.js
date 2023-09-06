8abf0f8a4f487b1c1103912320ff4b2c87c7557c
fghinzelli <fghinzelli@gmail.com>
2023-09-05 23:43:06 -0300

    Consign

--- mean/alurapic/app/routes/foto.js
+++ mean/alurapic/app/routes/foto.js
@@ -1,7 +1,5 @@
-var api = require('../api/foto');
-
 module.exports = function(app) {
-  
+  var api = app.api.fotos;
   app.get('/v1/fotos', api.lista)
 
 }
--- mean/alurapic/app/routes/grupos.js
+++ mean/alurapic/app/routes/grupos.js
@@ -1,5 +1,6 @@
 var api = require('../api/grupos')
 
 module.exports = function(app) {
+  var api = app.api.grupos;
   app.get('/v1/grupos', api.lista)
 }
