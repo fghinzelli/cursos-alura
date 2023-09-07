8853f826b50c03980cc05d2747ead10b3bf56f00
fghinzelli <fghinzelli@gmail.com>
2023-09-07 00:05:48 -0300

    Mongo

--- mean/alurapic/app/api/fotos.js
+++ mean/alurapic/app/api/fotos.js
@@ -1,39 +1,31 @@
-var api = {}
-var fotos = [
-  {_id: 1, titulo: 'Leão', url: 'https://cdn.leroymerlin.com.br/products/quadro_leao_bronze_100x100cm_90658085_0001_1800x1800.jpg'},
-  {_id: 2, titulo: 'Leão 2', url: 'https://cdn.leroymerlin.com.br/products/quadro_leao_bronze_100x100cm_90658085_0001_1800x1800.jpg'}
-];
+var mongoose = require('mongoose');
 
-var CONTADOR_ID = 2;
+var api = {}
+var model = mongoose.model('Foto');
 
 api.lista = (req, res) => {
-  
-
-  res.json(fotos);
+  model.find().then((error, fotos) => {
+    if (error) {
+      console.log(error)
+      res.status(500).json(error);
+    }
+    res.json(fotos)
+  })
 }
 
 api.buscarPorId = (req, res) => {
-  return res.json(fotos.filter(foto => foto._id == req.params.id)[0])
+  
 }
 
 api.removerPorId = (req, res) => {
-  fotos = fotos.filter(foto => foto._id !== req.params.id)
-  res.sendStatus(204)
+
 }
 
 api.adiciona = (req, res) => {
-  var foto = req.body;
-  foto._id = ++CONTADOR_ID;
-  fotos.push(foto);
-  res.json(foto);
+  
 }
 
 api.atualiza = (req, res) => {
-  var foto = req.body;
-  var indice = fotos.findIndex(foto => foto._id == req.params.id)
-  fotos[indice] = foto;
-  res.sendStatus(200);
+  
 }
-
 module.exports = api;
-
--- mean/alurapic/server.js
+++ mean/alurapic/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var app = require('./config/express');
 
-
+require('./config/database')('localhost/alurapic');
 
 http.createServer(app).listen(3000, () =>{
   console.log('Servidor rodando na porta 3000')
